135587dc006a75feb799cf694eb65694fa050d35
Whoistolu <digittolu25@gmail.com>
2021-12-01 23:07:27 +0100

    Create two new tables: owners and species then modified animals table

--- schema.sql
+++ schema.sql
@@ -1,12 +1,29 @@
 /* Database schema to keep the structure of entire database. */
-DROP table IF EXISTS animals;
+
+DROP TABLE IF EXISTS animals;
+DROP TABLE IF EXISTS owners;
+DROP TABLE IF EXISTS species;
+
+CREATE TABLE owners (
+    id SERIAL NOT NULL PRIMARY KEY,
+    full_name varchar(100),
+    AGE int
+);
+
+CREATE TABLE species (
+    id SERIAL NOT NULL PRIMARY KEY,
+    name varchar(100)
+);
 
 CREATE TABLE animals (
-    id serial primary key,
+    id SERIAL NOT NULL PRIMARY KEY,
     name varchar(100),
     date_of_birth date,
     escape_attempts int,
     neutered boolean,
     weight_kg decimal,
-    species varchar(100)
+    species_id INT,
+    owner_id INT, 
+    CONSTRAINT fk_species FOREIGN KEY(species_id) REFERENCES species(id),
+    CONSTRAINT fk_owners FOREIGN KEY(owner_id) REFERENCES owners(id)
 );
