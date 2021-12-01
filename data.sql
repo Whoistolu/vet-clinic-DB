2f3543f553b013eaea7cccf04ee8f3e2c7271b38
Whoistolu <digittolu25@gmail.com>
2021-12-01 23:18:08 +0100

    Modify animals table to include species_id and ownwers_id

--- data.sql
+++ data.sql
@@ -76,12 +76,16 @@ INSERT INTO animals (
   date_of_birth,
   escape_attempts,
   neutered,
+  species_id,
+  owner_id,
   weight_kg
 ) VALUES (
     'Agumon',
     '2020-2-3',
     0,
     true,
+    2,
+    1,
     10.23
 );
 
@@ -91,12 +95,16 @@ INSERT INTO animals (
   date_of_birth,
   escape_attempts,
   neutered,
+  species_id,
+  owner_id,
   weight_kg
 ) VALUES (
     'Gabumon',
     '2018-11-15',
     2,
     true,
+    2,
+    2,
     8
 );
 
@@ -106,12 +114,16 @@ INSERT INTO animals (
   date_of_birth,
   escape_attempts,
   neutered,
+  species_id,
+  owner_id,
   weight_kg
 ) VALUES (
     'Pikachu',
     '2021-1-7',
     1,
     false,
+    1,
+    2,
     15.04
 );
 -- Animal: Her name is Devimon. May 12th, 2017, 11kg. She is neutered and she has tried to escape 5 times.
@@ -120,12 +132,16 @@ INSERT INTO animals (
   date_of_birth,
   escape_attempts,
   neutered,
+  species_id,
+  owner_id,
   weight_kg
 ) VALUES (
     'Devimon',
     '2017-5-12',
     5,
     true,
+    2,
+    3,
     11
 );
 
@@ -135,12 +151,16 @@ INSERT into animals (
 	date_of_birth,
 	weight_kg,
 	neutered,
-	escape_attempts
+	species_id,
+  owner_id,
+  escape_attempts
 ) VALUES (
 	'Charmander',
 	'2020-02-08',
 	-11,
 	 false,
+   1,
+   4,
 	 0
 );
 
@@ -150,13 +170,17 @@ INSERT into animals (
 	date_of_birth,
 	weight_kg,
 	neutered,
-	escape_attempts
+	species_id,
+  owner_id,
+  escape_attempts
 ) VALUES (
 	'Plantmon',
 	'2022-11-15',
 	-5.7,
 	 true,
-	 2
+	 2,
+   3,
+   2
 );
 
 -- Animal: His name is Squirtle. He was born on Apr 2nd, 1993, and currently weighs -12.13kg. He was not neutered and he has tried to 3 times.
@@ -165,12 +189,16 @@ INSERT into animals (
 	date_of_birth,
 	weight_kg,
 	neutered,
-	escape_attempts
+	species_id,
+  owner_id,
+  escape_attempts
 ) VALUES (
 	'Squirtle',
 	'1993-04-02',
 	-12.13,
 	false,
+  1,
+  4,
 	3
 );
 
@@ -180,12 +208,16 @@ INSERT into animals (
 	date_of_birth,
 	weight_kg,
 	neutered,
-	escape_attempts
+	species_id,
+  owner_id,
+  escape_attempts
 ) VALUES (
 	'Angemon',
 	'2005-06-12',
 	-45,
 	true,
+  2,
+  5,
 	1
 );
 
@@ -195,12 +227,16 @@ INSERT into animals (
 	date_of_birth,
 	weight_kg,
 	neutered,
-	escape_attempts
+	species_id,
+  owner_id,
+  escape_attempts
 ) VALUES (
 	'Boarmon',
 	'2005-06-07',
 	20.4,
 	true,
+  2,
+  5,
 	7
 );
 
@@ -210,11 +246,18 @@ INSERT into animals (
 	date_of_birth,
 	weight_kg,
 	neutered,
-	escape_attempts
+	species_id,
+  owner_id,
+  escape_attempts
 ) VALUES (
 	'Blossom',
 	'1998-10-13',
 	17,
 	true,
+  1,
+  4,
 	3
 );
+
+
+COMMIT;
