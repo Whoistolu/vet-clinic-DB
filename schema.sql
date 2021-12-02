198b2354a890ee459f66a68971214b4634d355ee
Whoistolu <digittolu25@gmail.com>
2021-12-02 16:41:40 +0100

    Fix the id primary keys for each table

--- schema.sql
+++ schema.sql
@@ -5,18 +5,18 @@ DROP TABLE IF EXISTS owners;
 DROP TABLE IF EXISTS species;
 
 CREATE TABLE owners (
-    id SERIAL PRIMARY KEY,
+    id SERIAL NOT NULL PRIMARY KEY,
     full_name varchar(100),
     AGE int
 );
 
 CREATE TABLE species (
-    id SERIAL PRIMARY KEY,
+    id SERIAL NOT NULL PRIMARY KEY,
     name varchar(100)
 );
 
 CREATE TABLE animals (
-    id SERIAL PRIMARY KEY,
+    id SERIAL NOT NULL PRIMARY KEY,
     name varchar(100),
     date_of_birth date,
     escape_attempts int,
@@ -26,4 +26,3 @@ CREATE TABLE animals (
     owner_id INT, 
     CONSTRAINT fk_species FOREIGN KEY(species_id) REFERENCES species(id),
     CONSTRAINT fk_owners FOREIGN KEY(owner_id) REFERENCES owners(id)
-);
