68ad68d52d39efb3bef6b02f299cc99a2fb01945
Whoistolu <digittolu25@gmail.com>
2021-12-03 13:12:08 +0100

    Insert data for specializtions

--- data.sql
+++ data.sql
@@ -274,4 +274,13 @@ INSERT INTO vets
   ('Maisy Smith', 26, '2019-01-17'),
   ('Stephanie Mendez', 64, '1981-05-04'),
   ('Jack Harkness', 38, '2008-06-08');
-  
+
+  -- Insert data for specializations
+
+  INSERT INTO specializations 
+  (vet_id, species_id)
+VALUES 
+  (1, 1),
+  (3, 1),
+  (3, 2),
+  (4, 2);
