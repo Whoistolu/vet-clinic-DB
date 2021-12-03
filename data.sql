1c3df567854fcc2c70f9e2805fc073df07d0b719
Whoistolu <digittolu25@gmail.com>
2021-12-03 13:10:16 +0100

    Insert data for vets

--- data.sql
+++ data.sql
@@ -266,9 +266,12 @@ COMMIT;
 
 
 INSERT INTO vets
-  (name, age, date_of_graduation) 
-VALUES
+  (name,
+  age,
+  date_of_graduation
+) VALUES
   ('William Tatcher', 45, '2000-04-23'),
   ('Maisy Smith', 26, '2019-01-17'),
   ('Stephanie Mendez', 64, '1981-05-04'),
   ('Jack Harkness', 38, '2008-06-08');
+  
